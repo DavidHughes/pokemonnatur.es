c1f0aa54027ead5ef0634a832fa13f69316343ba
David Hughes <David.Hughes125@gmail.com>
2016-12-24 13:51:28 +0000

    Change some build stuff for GitHub Pages

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  return gulp.src('./sass/main.scss')
+  return gulp.src('./styles/main.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('.'));
+    .pipe(gulp.dest('./styles/'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./styles/**/*.scss', ['sass']);
 });
 
 gulp.task('default', ['sass']);
