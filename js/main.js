07be253486d5786ca94810e353fa6fb76eb70985
David Hughes <David.Hughes125@gmail.com>
2016-12-28 21:22:39 +0000

    Some code cleanup & commenting

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@
     const query = input.toUpperCase();
     const natures = Array.from(natureTable.querySelectorAll('tbody tr'));
 
+    // Reduce list to arrays of matching & non-matching natures.
     const queryResults = natures.reduce((results, nature) => {
       const natureName = nature.querySelector('td').textContent.toUpperCase();
 
@@ -26,20 +27,22 @@
       notMatching: []
     });
 
+    // Show/Hide based on query results.
     queryResults.matching.forEach(match => match.style.display = '');
     queryResults.notMatching.forEach(nonMatch => nonMatch.style.display = 'none');
 
-    let noMatchText = document.querySelector('p.no-match-found');
+    const noMatchText = document.querySelector('p.no-match-found') || document.createElement('p');
     if (!queryResults.matching.length) {
       // No results found - show 'no results' text & hide table.
-      if (!noMatchText) {
-        noMatchText = document.createElement('p');
+      if (!noMatchText.textContent.length) {
+        // Initialise 'No match found' text.
         noMatchText.classList.add('no-match-found');
         document.querySelector('main').appendChild(noMatchText);
       }
 
       noMatchText.textContent = `No nature containing "${input}" exists.`;
       noMatchText.style.display = '';
+
       natureTable.style.display = 'none';
     } else {
       // At least one match - show table, hide 'no results'.
@@ -56,9 +59,7 @@
   });
 
   if (window.location.hash) {
-    const query = window.location.hash.substr(1);
-    natureNameInput.value = query;
-    const e = new Event('keyup');
-    natureNameInput.dispatchEvent(e);
+    natureNameInput.value = window.location.hash.substr(1);;
+    natureNameInput.dispatchEvent(new Event('keyup'));
   }
 })();
